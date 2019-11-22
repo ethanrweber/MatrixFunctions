9916e7194215f3fb26d521c57e1f2801d8d94326
ethanrweber <ethanrweber@gmail.com>
2019-11-22 17:55:05 -0600

    Update
    add additional functions, update and add function summaries, remove extraneous brackets because I like how it looks

--- Program.cs
+++ Program.cs
@@ -2,25 +2,57 @@
 
 namespace MatrixFunctions
 {
-    class Program
+    /// <summary>
+    /// Library of matrix functions
+    /// available functions include:
+    ///     reduced row echelon form
+    ///     matrix addition/subtraction
+    ///     matrix multiplication
+    ///     get determinant
+    ///     get sub-matrix
+    /// </summary>
+    public static class Program
     {
         static void Main(string[] args)
         {
             decimal[,] m = {{1, -4, 2}, {-2, 8, -9}, {-1, 7, 0}};
+            decimal[,] m2 = {{1, 2, -1, -4}, {2, 3, -1, -11}, {-2, 0, -3, 22}};
             Console.WriteLine("m:");
             PrintMatrix(m);
             Console.WriteLine("rref(m):");
             PrintMatrix(RREF(m));
+
+            Console.WriteLine("m2:");
+            PrintMatrix(m2);
+            Console.WriteLine("rref(m2):");
+            PrintMatrix(RREF(m2));
+
+            Console.WriteLine("m + m:");
+            PrintMatrix(AddMatrices(m, m));
+
             Console.ReadLine();
+        }
+
+        private static decimal[,] _Deepcopy(decimal[,] matrix)
+        {
+            int n = matrix.GetLength(0), m = matrix.GetLength(1);
+
+            decimal[,] copy = new decimal[n,m];
+            for(int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    copy[i, j] = matrix[i, j];
+            return copy;
         }
 
         /// <summary>
         /// returns the given matrix in reduced row echelon form using Gaussian Elimination
         /// </summary>
-        /// <param name="matrix">n x m matrix</param>
+        /// <param name="A">n x m matrix</param>
         /// <returns>n x m matrix in reduced row echelon form</returns>
-        public static decimal[,] RREF(decimal[,] matrix)
+        public static decimal[,] RREF(decimal[,] A)
         {
+            decimal[,] matrix = _Deepcopy(A);
+
             int lead = 0, 
                 rowCount = matrix.GetLength(0), 
                 columnCount = matrix.GetLength(1);
@@ -38,7 +70,10 @@ namespace MatrixFunctions
                         i = r;
                         lead++;
                         if (columnCount == lead)
-                            return matrix;
+                        {
+                            lead--;
+                            break;
+                        }
                     }
                 }
                 // swap rows i and r
@@ -49,25 +84,18 @@ namespace MatrixFunctions
                     matrix[r, k] = temp;
                 }
 
-                if (matrix[r, lead] != 0)
-                {
+                var div = matrix[r, lead];
+                if (div != 0)
                     for (int k = 0; k < columnCount; k++)
-                    {
-                        matrix[i, k] /= matrix[r, lead];
-                    }
-                }
+                        matrix[r, k] /= div;
 
-                for (i = 0; i < rowCount; i++)
-                {
-                    // subtract row r * m[i, lead] from row i
-                    if (i != r)
+                for (int j = 0; j < rowCount; j++)
+                    if (j != r)
                     {
+                        var mult = matrix[j, lead];
                         for (int k = 0; k < columnCount; k++)
-                        {
-                            matrix[i, k] -= matrix[i, lead] * matrix[r, k];
-                        }
+                            matrix[j, k] -= mult * matrix[r, k];
                     }
-                }
 
                 lead++;
             }
@@ -75,17 +103,49 @@ namespace MatrixFunctions
             return matrix;
         }
 
-        public static void PrintMatrix<T>(T[,] matrix)
+        /// <summary>
+        /// prints a matrix to console output
+        /// </summary>
+        /// <param name="matrix">matrix to be printed</param>
+        /// <param name="round">number of decimal points to round answers, default 2</param>
+        public static void PrintMatrix(decimal[,] matrix, int round=2)
         {
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    Console.Write(matrix[i,j] + "\t");
-                }
+                    Console.Write(Math.Round(matrix[i,j], round) + "\t");
 
                 Console.WriteLine();
             }
+            Console.WriteLine();
+        }
+
+        public static decimal[,] AddMatrices(decimal[,] a, decimal[,] b) => _MatrixAdditionSubtraction(a, b, true);
+        public static decimal[,] SubtractMatrices(decimal[,] a, decimal[,] b) => _MatrixAdditionSubtraction(a, b, false);
+
+        /// <summary>
+        /// private method for matrix addition/subtraction with matrices a,b
+        /// addition or subtraction is determined by operation param (add = true, subtract = false)
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="operation"></param>
+        /// <returns></returns>
+        private static decimal[,] _MatrixAdditionSubtraction(decimal[,] a, decimal[,] b, bool operation)
+        {
+            if (a.GetLength(0) != b.GetLength(0) || a.GetLength(1) != b.GetLength(1))
+                throw new Exception("cannot add matrices of different sizes");
+
+            decimal[,] matrix = _Deepcopy(a);
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    if (operation)
+                        matrix[i, j] += b[i, j];
+                    else
+                        matrix[i, j] -= b[i, j];
+
+            return matrix;
         }
 
         public static int[,] MultiplyMatrices(int[,] a, int[,] b)
@@ -93,59 +153,52 @@ namespace MatrixFunctions
             if(a.GetLength(1) != b.GetLength(0))
                 throw new Exception("cannot multiply matrices of different sizes");
 
-
             int m = a.GetLength(0), n = b.GetLength(1);
             int[,] result = new int[a.GetLength(0), b.GetLength(1)];
 
 
-            // iterate through result rows
             for(int i = 0; i < m; i++)
-            {
-                // iterate through result columns
                 for(int j = 0; j < n; j++)
                 {
                     int sum = 0;
                     for (int k = 0; k < m; k++)
-                    {
                         sum += a[i, k] * b[k, j];
-                    }
                     result[i, j] = sum;
                 }
-            }
 
             return result;
         }
 
-        public static int GetDeterminant(int[,] matrix)
+        public static decimal GetDeterminant(decimal[,] matrix)
         {
             int n = matrix.GetLength(0);
 
-            if(n == 0)
-                throw new Exception("matrix must have values");
-            if (n == 1)
-                return matrix[0, 0];
+            if (n == 0) throw new Exception("matrix cannot be empty");
+            if (n == 1) return matrix[0, 0];
 
-            int det = 0;
-            for (int j = 0; j < n; j++)
-            {
-                det += (int) Math.Pow(-1, 1 + j) * matrix[1, j] * GetDeterminant(GetSubmatrix(matrix, 1, j));
-            }
-
+            decimal det = 0;
+            for(int j = 0; j < n; j++)
+                det += (decimal)Math.Pow(-1, 1 + j) * matrix[1, j] * GetDeterminant(GetSubmatrix(matrix, 1, j));
+            
             return det;
         }
 
-        public static int[,] GetSubmatrix(int[,] parent, int row, int col)
+        /// <summary>
+        /// retrieves a submatrix of the parent matrix, useful for Determinant calculations
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <returns>returns an (m-1) x (n-1) sub-matrix excluding row 'row' and column 'col'</returns>
+        public static decimal[,] GetSubmatrix(decimal[,] parent, int row, int col)
         {
             int pn = parent.GetLength(0);
-            if(pn == 0)
-                throw new Exception("matrix must have values");
-            if(pn == 1)
-                return new int[0,0];
+            if (pn == 0) throw new Exception("matrix must have values");
+            if (pn == 1) return new decimal[0,0];
 
-            int n = pn - 1;
-            int[,] matrix = new int[n,n];
-            int rOffset = 0, cOffset = 0;
+            int n = pn - 1, rOffset = 0, cOffset = 0;
 
+            decimal[,] matrix = new decimal[n, n];
             for (int i = 0; i < n; i++)
             {
                 if (row == i)
